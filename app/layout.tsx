95bd4b917e54460886e413debf86e89ad29c6cc5
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2026-05-24 13:56:23 -0700

    feat: og image size changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export const metadata: Metadata = {
       {
         url: "/og-image.jpg",
         width: 1200,
-        height: 630,
+        height: 1030,
         alt: "Escápate a San Carlos",
       },
     ],
